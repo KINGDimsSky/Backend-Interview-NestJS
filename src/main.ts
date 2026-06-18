ca297532a0538f1950c5a454041f301f0cf554c3
KINGDimsSky <dimasmarcelo2004@gmail.com>
2026-06-18 10:57:29 +0700

    Finishing

--- src/main.ts
+++ src/main.ts
@@ -8,26 +8,22 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 1. Set Global API Prefix
   app.setGlobalPrefix('api');
 
-  // 2. Pasang Global Filter & Interceptor yang baru kita buat
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
 
-  // 3. Aktifkan Validasi Otomatis (class-validator) dengan Strict Mode
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,        // Membuang properti yang tidak ada di DTO
-    transform: true,        // Otomatis mengubah tipe data (misal string "1" ke number 1)
-    forbidNonWhitelisted: true, // Melempar error jika client mengirim properti ilegal
+    whitelist: true,        
+    transform: true,        
+    forbidNonWhitelisted: true,
   }));
 
-  // 4. Setup Swagger Dokumentasi Otomatis (Nilai Plus di mata Interviewer)
   const config = new DocumentBuilder()
     .setTitle('Laundry REST API')
     .setDescription('Dokumentasi API Sistem Manajemen Laundry Profesional')
     .setVersion('1.0')
-    .addBearerAuth() // Aktifkan opsi input token JWT di UI Swagger
+    .addBearerAuth() 
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
